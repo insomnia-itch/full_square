886a6a3697786841de80d6d4c8cc98be2d7ce3cb
Jelani Woods <jelani@firstdraft.com>
2021-06-12 16:59:28 -0500

    Player horizontal movement (#20)
    * Basic player movement
    * Enforce max speed

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [Header("Horizontal Movement")]
+    public float moveSpeed = 10f;
+    public Vector2 direction;
+    //private bool facingRight = true;
+
+    [Header("Components")]
+    public Rigidbody2D rb;
+
+
+    [Header("Physics")]
+    public float maxSpeed = 12f;
+
+    //[Header("Collision")]
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalDirection = Input.GetAxisRaw("Horizontal");
+        float verticalDirection = Input.GetAxisRaw("Vertical");
+        direction = new Vector2(horizontalDirection, verticalDirection);
+    }
+
+    void movePlayer(float horizontal)
+    {
+        Vector2 horizonatlForce = Vector2.right * horizontal * moveSpeed;
+        rb.AddForce(horizonatlForce);
+
+        if(Mathf.Abs(rb.velocity.x) > maxSpeed) {
+            float playerDirection = Mathf.Sign(rb.velocity.x);
+            rb.velocity = new Vector2(playerDirection * maxSpeed, rb.velocity.y);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        movePlayer(direction.x);
+    }
+}
